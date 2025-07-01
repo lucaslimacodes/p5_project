e7e25c8af04c63043234ce5a9b4cf4acb78966a5
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2025-07-01 01:40:25 -0300

    feat(dfs): Adds DFS implementation

---
+++ src/algorithms/dfs.js
@@ -0,0 +1,57 @@
+class DFS extends Algorithm {
+    constructor(grid, startPosition, endPosition){
+        super(grid,startPosition,endPosition)
+        this.frontier = [startPosition] // queue that stores p5.vectors
+    }
+
+
+    runStep(){
+        // check if algorithm has finished
+        if(this.status == SUCCESS || this.status == FAILURE){
+            return 
+        }
+        // if the frontier is empty, the search failed
+        if(this.frontier.length == 0){
+            this.status = FAILURE   
+            return
+        }
+
+        let state = this.frontier.pop()
+        this.explored.push(state)
+
+        if(state.equals(this.endPosition)){
+            this.status = SUCCESS
+            return
+        }
+
+        let neighbors = this.getNeighbors(state)
+
+        for(let i=0;i<neighbors.length;i++){
+            if(this.isPositionInFrontier(neighbors[i]) == false && this.wasPositionExplored(neighbors[i]) == false){
+                this.frontier.push(neighbors[i])
+                this.cameFrom[neighbors[i]] = state
+            }
+        }
+    }
+
+
+    isPositionInFrontier(position){
+        for(let i=0;i<this.frontier.length;i++){
+            if(this.frontier[i].equals(position)){
+                return true
+            }
+        }
+        return false
+    }
+
+    reset(startPosition, endPosition, grid){
+        super.reset(startPosition, endPosition, grid)
+        this.frontier = [this.startPosition] 
+    }
+    
+    getFrontierArray(){
+        return this.frontier
+    }
+
+
+}
