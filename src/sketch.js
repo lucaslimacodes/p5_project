32979b0cb11d8ed17dd01919b9bc929e6209decb
Vitor Manoel <vitorearthur15@hotmail.com>
2025-07-01 06:47:44 -0300

    feature(buttons): adding start/stop/reset buttons

--- src/sketch.js
+++ src/sketch.js
@@ -4,25 +4,29 @@ let currAlgorithm
 let WIDTH
 let HEIGHT
 const GRID_SIZE = 40
+var start = false
+var stopped = false
 
 // runs currAlgorithm based on world
 // this function not only runs the algorithm exploration, but it is also responsible for managing the animation of the agent heading towards the food
 function runAlgorithm(){
-  if(currAlgorithm.status == INPROGRESS){ // algorithm is trying to find the food while updates the frontier and explored variables to world object 
-   currAlgorithm.runStep()
-    world.frontier = currAlgorithm.getFrontierArray()// update frontier array
-    world.explored = currAlgorithm.explored // update explored array
-  }
-  else if(currAlgorithm.status == FAILURE){ // algorithm could not find food, resets world and algorithm
-    world = new World(WIDTH,HEIGHT,GRID_SIZE)
-    currAlgorithm.reset(world.agentPosition, world.foodPosition, world.tiles)
-  }
-  else{ // algorithm found a valid path
-    world.resetAlgorithmArrays() // stop drawing frontier and explored tiles
-    world.path = currAlgorithm.getPath() // start drawing path and animation
-    if(world.agentPosition.equals(world.foodPosition)){ // animation end
-      world.resetWorldStateToNewFoodPosition() // finds a new food position
-      currAlgorithm.reset(world.agentPosition, world.foodPosition, world.tiles) // reset algotithm to run again
+  if(start && !stopped) {
+    if(currAlgorithm.status == INPROGRESS){ // algorithm is trying to find the food while updates the frontier and explored variables to world object 
+    currAlgorithm.runStep()
+      world.frontier = currAlgorithm.getFrontierArray()// update frontier array
+      world.explored = currAlgorithm.explored // update explored array
+    }
+    else if(currAlgorithm.status == FAILURE){ // algorithm could not find food, resets world and algorithm
+      world = new World(WIDTH,HEIGHT,GRID_SIZE)
+      currAlgorithm.reset(world.agentPosition, world.foodPosition, world.tiles)
+    }
+    else{ // algorithm found a valid path
+      world.resetAlgorithmArrays() // stop drawing frontier and explored tiles
+      world.path = currAlgorithm.getPath() // start drawing path and animation
+      if(world.agentPosition.equals(world.foodPosition)){ // animation end
+        world.resetWorldStateToNewFoodPosition() // finds a new food position
+        currAlgorithm.reset(world.agentPosition, world.foodPosition, world.tiles) // reset algotithm to run again
+      }
     }
   }
 }
@@ -35,6 +39,9 @@ function setup() {
   let canva = createCanvas(WIDTH, HEIGHT);
   world = new World(WIDTH,HEIGHT,GRID_SIZE)
   const combo = document.getElementById("combo");
+  const startbutton = document.getElementById("startbutton")
+  const stopbutton = document.getElementById("stopbutton")
+  const resetbutton = document.getElementById("resetbutton")
 
   if(combo.value == "BFS") currAlgorithm = new BFS(world.tiles, world.agentPosition, world.foodPosition)
   if(combo.value == "DFS") currAlgorithm = new DFS(world.tiles, world.agentPosition, world.foodPosition)
@@ -71,7 +78,21 @@ function setup() {
     currAlgorithm.reset(world.agentPosition, world.foodPosition, world.tiles)
   })
 
-
+  startbutton.addEventListener("click", () => {
+    start = true
+  })
+  stopbutton.addEventListener("click", () => {
+    if(start) {
+      stopped = true
+    }
+  })
+  resetbutton.addEventListener("click", () => {
+    canva = createCanvas(WIDTH, HEIGHT);
+    world = new World(WIDTH,HEIGHT,GRID_SIZE)
+    currAlgorithm.reset(world.agentPosition, world.foodPosition, world.tiles)
+    start = false
+    stopped = false
+  })
 
 }
 
